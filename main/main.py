21938623adae07dac7c720dceafb106605f7af1c
Somoskői Gábor <somoskoi.gabor@gmail.com>
2025-07-30 15:57:22 +0200

    Leegyszerűsítettem a címke alapján kereső függvényt, csak Windows Kitöröltem a meghajtók nevét megadó függvényt

--- main/main.py
+++ main/main.py
@@ -6,36 +6,18 @@ computer_name = socket.gethostname()
 
 def find_usb_path_by_label(label):
     """
-    Különböző operációs rendszerek esetén megmondja,
-    hogy mi egy adott (label) meghajtó elérési útvonala
+    Megmondja, hogy mi egy adott (label) meghajtó elérési útvonala
     """
-    system = platform.system()
 
-    if system == "Windows":
-        import win32api
-        drives = win32api.GetLogicalDriveStrings().split('\000')[:-1]
-        for drive in drives:
-            try:
-                volume_name = win32api.GetVolumeInformation(drive)[0]
-                if volume_name == label:
-                    return drive
-            except:
-                continue
-
-    elif system == "Linux":
-        base_path = f"/media/{os.getlogin()}"
-        if os.path.exists(base_path):
-            for name in os.listdir(base_path):
-                if name == label:
-                    return os.path.join(base_path, name)
-
-    elif system == "Darwin":  # macOS
-        base_path = "/Volumes"
-        for name in os.listdir(base_path):
-            if name == label:
-                return os.path.join(base_path, name)
-
-    return None
+    import win32api
+    drives = win32api.GetLogicalDriveStrings().split('\000')[:-1]
+    for drive in drives:
+        try:
+            volume_name = win32api.GetVolumeInformation(drive)[0]
+            if volume_name == label:
+                return drive
+        except:
+            return None
 
 def newest_subdir(path):
     """Egy könyvtárban meghatározza a legfrissebb alkönyvtár elérését
@@ -52,13 +34,12 @@ def newest_subdir(path):
     # Módosítási idő alapján rendezés
     return max(alkonyvtarak, key=os.path.getmtime)
 
-
 # Example usage
 usb_label = "MyPassport"
 usb_path = find_usb_path_by_label(usb_label)
 print("USB Path:", usb_path)
 print("Computer Name:", computer_name)
-print(drives)
+
 
 mappa = 'C:/Users/Somoskői Gábor/Backup/Calibre/Job-202410161007548'
 print("Legfrissebb alkönyvtár:", newest_subdir(mappa))
