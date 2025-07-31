5a00f6b66b63040a6885e62c61c8d478bb92700b
Somoskői Gábor <somoskoi.gabor@gmail.com>
2025-07-31 12:34:34 +0200

    Létrehoztam a Somoskoigabor gépen mentendő mappák dictionary-át

---
+++ data/adat.py
@@ -0,0 +1,7 @@
+# Fájlok, amit menteni kell Somoskoigabor gépen
+SOURCE_SOMOSKOIGABOR = {
+    "calibre": "C:/Users/Somoskői Gábor/Backup/Calibre/Job-202410161007548",
+    "obsidianDaily": "C:/Users/Somoskői Gábor/Backup/Obsidian/ObsidianDaily/Job-202410151128605",
+    "obsidianWeekly": "C:/Users/Somoskői Gábor/Backup/Obsidian/ObsidianWeekly/Job-202410151144164"
+}
+    
